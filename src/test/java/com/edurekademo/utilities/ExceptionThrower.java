1fb8c1109814d563d8e144ea96a15e0114569a3b
sangeethak92 <58594758+sangeethak92@users.noreply.github.com>
2020-03-17 09:43:09 +0000

    Update ExceptionThrower.java

--- src/test/java/com/edurekademo/utilities/ExceptionThrower.java
+++ src/test/java/com/edurekademo/utilities/ExceptionThrower.java
@@ -15,7 +15,7 @@ public class ExceptionThrower {
         }
         
         public void doNothing() throws IOException {
-                throw new IOException("TESTIOEXCEPTION"); 
+                throw new IOException("TESTIOEXCEPTION")
         }
         
         private void doXXX() { 
